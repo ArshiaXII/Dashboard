8f0e9b89782f852d4292094ec22813fb7f7ff552
Arshia <82704919+ArshiaXII@users.noreply.github.com>
2025-03-01 23:50:46 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,26 +51,25 @@ module.exports = {
           DEFAULT: "hsl(var(--card))",
           foreground: "hsl(var(--card-foreground))",
         },
-        primary: "#003366",
-        secondary: "#FFD700",
       },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      fontFamily: {
-        playfair: ["Playfair Display", "serif"],
-        sans: ["Open Sans", "sans-serif"],
-      },
       keyframes: {
-        fadeIn: {
-          "0%": { opacity: "0" },
-          "100%": { opacity: "1" },
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
         },
       },
       animation: {
-        fadeIn: "fadeIn 0.3s ease-in-out",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
